de5e9630024227d7f688f0f7ac7f29b9bcec2ff9
belcortes <belcortes@gmail.com>
2018-03-30 15:33:20 -0700

    setting up test environment

--- src/main/resources/db/migration/V1__Create_users_table.sql
+++ src/main/resources/db/migration/V1__Create_users_table.sql
@@ -3,8 +3,6 @@ create table USERS (
     USER_NAME varchar(100) NOT NULL,
     FIRST_NAME varchar(100) NOT NULL,
     LAST_NAME varchar(100) NOT NULL,
-    LAST_LOGIN_AT timestamp,
     LAST_SEARCH varchar(100) NOT NULL,
-    LAST_SEARCH_AT timestamp,
     ADMIN boolean
 );
---
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,5 @@
+INSERT INTO USERS
+  (USER_NAME, FIRST_NAME, LAST_NAME, LAST_SEARCH, ADMIN)
+VALUES
+  ('username', 'First', 'User', 'meetings', false),
+  ('username', 'First', 'Admin', '', true);
