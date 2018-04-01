cbc7bd01bedd7e4eb75231f70a295dd07b11d4ef
belcortes <belcortes@gmail.com>
2018-04-01 15:35:43 -0700

    fixed Iterables error

--- src/test/java/com/example/findmenyc/repositories/UserRepositoryTest.java
+++ src/test/java/com/example/findmenyc/repositories/UserRepositoryTest.java
@@ -11,10 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-
 import static org.hamcrest.Matchers.is;
-
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
