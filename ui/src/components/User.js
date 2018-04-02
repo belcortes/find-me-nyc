b54c9423726a0f809444b9bb0568be11412f8b4f
belcortes <belcortes@gmail.com>
2018-04-02 10:31:10 -0700

    small syntax error

--- ui/src/components/User.js
+++ ui/src/components/User.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const User = (props) => {
   return (
-    <h2>User Component</h2>
-    <div id={`user-${props.user.id}`} data-user-display>
-      <div id={`user-${props.user.id}-user-name`}>
-        {props.user.userName}
+    <div>
+      <h2>User Component</h2>
+      <div id={`user-${props.user.id}`} data-user-display>
+        <div id={`user-${props.user.id}-user-name`}>
+          {props.user.userName}
+        </div>
       </div>
     </div>
   )
