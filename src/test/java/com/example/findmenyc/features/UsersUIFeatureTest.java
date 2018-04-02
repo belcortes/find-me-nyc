c639d9a0ae77a7eb85244ef68d7bf80aa55a4da4
belcortes <belcortes@gmail.com>
2018-04-02 09:02:05 -0700

    setting up UI tests

---
+++ src/test/java/com/example/findmenyc/features/UsersUIFeatureTest.java
@@ -0,0 +1,27 @@
+package com.example.findmenyc.features;
+
+import com.example.findmenyc.repositories.UserRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class UsersUIFeatureTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @After
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
+}
