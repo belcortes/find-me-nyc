97599151454c64c37e1cddca63da669d2c4b24e7
belcortes <belcortes@gmail.com>
2018-04-09 11:46:14 -0700

    adding results api;

---
+++ results-api/src/main/resources/db/migration/V2__Create_results_table.sql
@@ -0,0 +1,9 @@
+create table RESULTS (
+    ID serial,
+    LOCATION varchar(100) NOT NULL,
+    LAT decimal(9,6) NOT NULL,
+    LNG decimal(9,6) NOT NULL,
+    SECTION varchar(100),
+    DESCRIPTION text,
+    DATE_AND_TIME varchar(100)
+);
